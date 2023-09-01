4f4a3c94404a81d235d3ff72ccccd677efe66a41
Robert Murvai <robert.murvai@bullguard.com>
2023-09-01 17:23:12 +0300

    refactor mqttdata.js

--- WebApp/js/mqttdata.js
+++ WebApp/js/mqttdata.js
@@ -20,41 +20,21 @@ function Login()
 
 
 
-
 var max_sample = 30;
-var map_time = new Array();
-map_time.push(0);
+var map_time = [0];
+var map_timeCO = [0];
+var map_timeLPG = [0];
+var map_timeSmoke = [0];
+var map_timePir = [0];
+var map_temp = [0];
+var map_humidity = [0];
+var map_lpg = [{ x: new Date(), y: 0 }];
+var map_co = [{ x: new Date(), y: 0 }];
+var map_smoke = [{ x: new Date(), y: 0 }];
+var map_pir = [{ x: new Date(), y: 0 }];
 
-var map_timeCO = new Array();
-map_timeCO.push(0);
 
-var map_timeLPG = new Array();
-map_timeLPG.push(0);
-
-var map_timeSmoke = new Array();
-map_timeSmoke.push(0);
-
-var map_timePir = new Array();
-map_timePir.push(0);
-
-var map_temp = new Array();
-map_temp.push(0);
-
-var map_humidity = new Array();
-map_humidity.push(0);
-
-var map_lpg = new Array();
-map_lpg.push({ x: new Date(), y: 0 });
-
-var map_co = new Array();
-map_co.push({ x: new Date(), y: 0 });
-
-var map_smoke = new Array();
-map_smoke.push({ x: new Date(), y: 0 });
-
-var map_pir = new Array();
-map_lpg.push({ x: new Date(), y: 0 });
-
+var chartCO, chartLPG, chartSmoke, chartTemp, chartPir;
 
 function CreateChartTemp() {
     var ctx = document.getElementById('tempChart').getContext('2d');
@@ -111,20 +91,19 @@ function CreateChartTemp() {
     });
     return myChart;
 }
-var chartTemp;
 
-function CreateChartCO() {
-    var ctx = document.getElementById('coChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
+// Function for create a line chart
+function createChart(ctx, type, labels, data, label, yAxisLabel) {
+    return new Chart(ctx, {
+        type: type,
         data: {
-            labels: map_timeCO,
+            labels: labels,
             datasets: [{
-                data: map_co,
-                label: 'CO ppm',
+                data: data,
+                label: label,
                 type: 'line',
                 backgroundColor: 'rgba(0,0,0,0.1)',
-                borderColor:'rgba(0,0,0,0)'
+                borderColor: 'rgba(0,0,0,0)'
             }]
         },
         options: {
@@ -134,7 +113,7 @@ function CreateChartCO() {
                 xAxes: [{
                     scaleLabel: {
                         display: true,
-                        lavelString:'Seconds'
+                        labelString: 'Seconds'
                     },
                     ticks: {
                         maxRotation: 90,
@@ -144,97 +123,13 @@ function CreateChartCO() {
                 yAxes: {
                     scaleLabel: {
                         display: true,
-                        labelString: 'CO ppm'
-                    },
-                }
-            }
-        }
-    });
-    return chart;
-}
-var chartCO;
-
-function CreateChartLPG() {
-    var ctx = document.getElementById('lpgChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: map_timeLPG,
-            datasets: [{
-                data: map_lpg,
-                label: 'LPG ppm',
-                type: 'line',
-                backgroundColor: 'rgba(0,0,0,0.1)',
-                borderColor:'rgba(0,0,0,0)'
-            }]
-        },
-        options: {
-            animation: false,
-            responsive: true,
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        lavelString:'Seconds'
-                    },
-                    ticks: {
-                        maxRotation: 90,
-                        minRotation: 90
+                        labelString: yAxisLabel
                     }
-                }],
-                yAxes: {
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'LPG ppm'
-                    },
                 }
             }
         }
     });
-    return chart;
 }
-var chartLPG;
-
-function CreateChartSmoke() {
-    var ctx = document.getElementById('smokeChart').getContext('2d');
-        var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: map_timeSmoke,
-            datasets: [{
-                data: map_lpg,
-                label: 'Smoke ppm',
-                type: 'line',
-                backgroundColor: 'rgba(0,0,0,0.1)',
-                borderColor:'rgba(0,0,0,0)'
-            }]
-        },
-        options: {
-            animation: false,
-            responsive: true,
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        lavelString:'Seconds'
-                    },
-                    ticks: {
-                        maxRotation: 90,
-                        minRotation: 90
-                    }
-                }],
-                yAxes: {
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Smoke ppm'
-                    },
-                }
-            }
-        }
-    });
-    return chart;
-}
-var chartSmoke;
 
 function CreateChartPir() {
     var ctx = document.getElementById('pirChart').getContext('2d');
@@ -281,7 +176,14 @@ function CreateChartPir() {
     });
     return chart;
 }
-var chartPir;
+
+function addValueToMap(map, value) {
+    if (map.length >= max_sample) {
+        map.shift();
+    }
+    map.push(value);
+    return map;
+}
 
 var co_state = true;
 function onoff_co() {
@@ -456,228 +358,38 @@ function ConnectionLost(res) {
 	}
 }
 
-function DisplayHumidityMeasurement(data) {
+// Function to display measurment
+function displayMeasurement(id, data, text) {
     var isAtRisk = false;
     if (data == undefined) {
-        $('#humidity_safe').hide();
-        $('#humidity_ignored').show();
-        $('#humidity_risk').hide();
-        $('#humidity').hide();
-        $('#humidity_text_off').show();
-        
+        $(`#${id}_safe`).hide();
+        $(`#${id}_ignored`).show();
+        $(`#${id}_risk`).hide();
+        $(`#${id}`).hide();
+        $(`#${id}_text_off`).show();
+
         isAtRisk = false;
-    }
-    else {
-        var humidity_text = 'Humidity is {0}%';
-        humidity_text = humidity_text.replace('{0}', data.value);
-        $('#humidity').text(humidity_text);
+    } else {
+        //var text = `${unit} concentration is ${data.value}`;
+        $(`#${id}`).html(text);
         if (data.risk) {
-        
-            $('#humidity_safe').hide();
-            $('#humidity_ignored').hide();
-            $('#humidity_risk').show();
-            $('#humidity').show();
-            $('#humidity_text_off').hide();
-            
+            $(`#${id}_safe`).hide();
+            $(`#${id}_ignored`).hide();
+            $(`#${id}_risk`).show();
+            $(`#${id}`).show();
+            $(`#${id}_text_off`).hide();
             isAtRisk = true;
-        }
-        else {
-            $('#humidity_safe').show();
-            $('#humidity_ignored').hide();
-            $('#humidity_risk').hide();
-            $('#humidity').show();
-            $('#humidity_text_off').hide();
-            
+        } else {
+            $(`#${id}_safe`).show();
+            $(`#${id}_ignored`).hide();
+            $(`#${id}_risk`).hide();
+            $(`#${id}`).show();
+            $(`#${id}_text_off`).hide();
             isAtRisk = false;
         }
     }
     return isAtRisk;
 }
-
-function DisplayTemperatureMeasurement(data) {
-    var isAtRisk = false;
-    if (data == undefined) {
-        $('#temperature_safe').hide();
-        $('#temperature_ignored').show();
-        $('#temperature_risk').hide();
-        $('#temperature').hide();
-        $('#temperature_text_off').show();
-        
-        isAtRisk = false;
-    }
-    else {
-        var temperature_text = 'Temperature is {0} &#8451;';
-        temperature_text = temperature_text.replace('{0}', data.value);
-        $('#temperature').html(temperature_text);
-        if (data.risk) {
-        
-            $('#temperature_safe').hide();
-            $('#temperature_ignored').hide();
-            $('#temperature_risk').show();
-            $('#temperature').show();
-            $('#temperature_text_off').hide();
-            isAtRisk = true;
-        }
-        else {
-            $('#temperature_safe').show();
-            $('#temperature_ignored').hide();
-            $('#temperature_risk').hide();
-            $('#temperature').show();
-            $('#temperature_text_off').hide();
-            isAtRisk = false;
-        }
-    }
-    return isAtRisk;
-}
-
-function DisplayLPGMeasurement(data)
-{
-    isAtRisk = false;
-    if (data == undefined) {
-        $('#lpg_safe').hide();
-        $('#lpg_ignored').show();
-        $('#lpg_risk').hide();
-        $('#lpg').hide();
-        $('#lpg_text_off').show();
-        
-        isAtRisk = false;
-    }
-    else {
-        var text = 'LPG ppm concentration is {0}';
-        text = text.replace('{0}', data.value);
-        $('#lpg').html(text);
-        if (data.risk){
-            $('#lpg_safe').hide();
-            $('#lpg_ignored').hide();
-            $('#lpg_risk').show();
-            $('#lpg').show();
-            $('#lpg_text_off').hide();    
-            isAtRisk = true;
-        }
-        else {
-            $('#lpg_safe').show();
-            $('#lpg_ignored').hide();
-            $('#lpg_risk').hide();
-            $('#lpg').show();
-            $('#lpg_text_off').hide();
-            isAtRisk = false;
-        }
-    }
-    return isAtRisk;
-}
-
-function DisplaySmokeMeasurement(data)
-{
-    var isAtRisk = false;
-    if (data == undefined) {
-        $('#smoke_safe').hide();
-        $('#smoke_ignored').show();
-        $('#smoke_risk').hide();
-        $('#smoke').hide();
-        $('#smoke_text_off').show();
-        
-        isAtRisk = false;
-    }
-    else {
-        var text = 'Smoke ppm concentration is {0}';
-        text = text.replace('{0}', data.value);
-        $('#smoke').html(text);
-        if (data.risk){
-        
-            $('#smoke_safe').hide();
-            $('#smoke_ignored').hide();
-            $('#smoke_risk').show();
-            $('#smoke').show();
-            $('#smoke_text_off').hide();    
-            isAtRisk = true;
-        }
-        else {
-            $('#smoke_safe').show();
-            $('#smoke_ignored').hide();
-            $('#smoke_risk').hide();
-            $('#smoke').show();
-            $('#smoke_text_off').hide();
-            isAtRisk = false;
-        }
-    }
-    return isAtRisk;
-}
-
-function DisplayCOMeasurement(data)
-{
-    var isAtRisk = false;
-    if (data == undefined) {
-        $('#co_safe').hide();
-        $('#co_ignored').show();
-        $('#co_risk').hide();
-        $('#co').hide();
-        $('#co_text_off').show();
-        
-        isAtRisk = false;
-    }
-    else {
-        //https://www.kidde.com/home-safety/en/us/support/help-center/browse-articles/articles/what-are-the-carbon-monoxide-levels-that-will-sound-the-alarm.html
-        var text = 'CO ppm concentration is {0}';
-        text = text.replace('{0}', data.value);
-        $('#co').html(text);
-        if (data.risk) {
-        
-            $('#co_safe').hide();
-            $('#co_ignored').hide();
-            $('#co_risk').show();
-            $('#co').show();
-            $('#co_text_off').hide();    
-            isAtRisk = true;
-        }
-        else {
-            $('#co_safe').show();
-            $('#co_ignored').hide();
-            $('#co_risk').hide();
-            $('#co').show();
-            $('#co_text_off').hide();
-            isAtRisk = false;
-        }
-    }
-    return isAtRisk;
-}
-
-function DisplayPirMeasurement(data)
-{
-    var isAtRisk = false;
-    if (data == undefined) {
-        $('#pir_safe').hide();
-        $('#pir_ignored').show();
-        $('#pir_risk').hide();
-        $('#pir').hide();
-        $('#pir_text_off').show();
-        
-        isAtRisk = false;
-    }
-    else {
-        var text = 'Movement is {0}';
-        text = text.replace('{0}', data.value);
-        $('#pir').html(text);
-        if (data.risk) {
-        
-            $('#pir_safe').hide();
-            $('#pir_ignored').hide();
-            $('#pir_risk').show();
-            $('#pir').show();
-            $('#pir_text_off').hide();    
-            isAtRisk = true;
-        }
-        else {
-            $('#pir_safe').show();
-            $('#pir_ignored').hide();
-            $('#pir_risk').hide();
-            $('#pir').show();
-            $('#pir_text_off').hide();
-            isAtRisk = false;
-        }
-    }
-    return isAtRisk;
-}
-
 
 /*Callback for incoming message processing */
 function MessageArrived(message) {
@@ -687,78 +399,56 @@ function MessageArrived(message) {
     var isAtRisk = false;
     var aux;
     if (hum_state) {
-        aux = DisplayHumidityMeasurement(obj.dht22.humidity);
-        //map_humidity = AddValueToMap(map_humidity, obj.dht22.humidity.value);
+        aux = displayMeasurement('humidity', obj.dht22.humidity, `Humidity is ${obj.dht22.humidity.value}%`);
         isAtRisk = isAtRisk || aux;
     }
     if (temp_state) {
-        aux = DisplayTemperatureMeasurement(obj.dht22.temperature);
-        
-        
+        aux = displayMeasurement('temperature', obj.dht22.temperature, `Temperature is ${obj.dht22.temperature.value}&#8451;`);
         isAtRisk = isAtRisk || aux;
     }
     if (temp_state || hum_state)
     {
-        if (map_temp.length >= max_sample) {
-            map_temp.shift();
-            map_humidity.shift();
-            map_time.shift();
-        }
-        map_temp.push(obj.dht22.temperature.value);
-        map_humidity.push(obj.dht22.humidity.value);
-        map_time.push(moment().format('LTS'));
+        map_temp = addValueToMap(map_temp, obj.dht22.temperature.value);
+        map_humidity = addValueToMap(map_humidity, obj.dht22.humidity.value);
+        map_time = addValueToMap(map_time, moment().format('LTS'));
 
         chartTemp.update();
     }
 
     if (lpg_state) {
-        aux = DisplayLPGMeasurement(obj.mq2.lpg);
-        if (map_lpg.length >= max_sample) {
-            map_lpg.shift();
-            map_timeLPG.shift();
-        }
-        map_lpg.push(obj.mq2.lpg.value);
-        map_timeLPG.push(moment().format('LTS'));
+        aux = displayMeasurement('lpg', obj.mq2.lpg, `LPG ppm concentration is ${obj.mq2.lpg.value}`);
+        
+        map_lpg = addValueToMap(map_lpg, obj.mq2.lpg.value);
+        map_timeLPG = addValueToMap(map_timeLPG, moment().format('LTS'));
 
         chartLPG.update();
         isAtRisk = isAtRisk || aux;
     }
     if (smoke_state) {
-        aux = DisplaySmokeMeasurement(obj.mq2.smoke);
-        if (map_smoke.length >= max_sample) {
-            map_smoke.shift();
-            map_timeSmoke.shift();
-        }
-        map_smoke.push(obj.mq2.smoke.value);
-        map_timeSmoke.push(moment().format('LTS'));
+        aux = displayMeasurement('smoke', obj.mq2.smoke, `Smoke ppm concentration is ${obj.mq2.smoke.value}`);
+       
+        map_smoke = addValueToMap(map_smoke, obj.mq2.smoke.value);
+        map_timeSmoke = addValueToMap(map_timeLPG, moment().format('LTS'));
 
         chartSmoke.update();
         isAtRisk = isAtRisk || aux;
     }
     if (co_state) {
-        aux = DisplayCOMeasurement(obj.mq2.co);
-        if (map_co.length >= max_sample) {
-            map_co.shift();
-            map_timeCO.shift();
-        }
-        map_co.push(obj.mq2.co.value);
-        map_timeCO.push(moment().format('LTS'));
+        aux = displayMeasurement('co', obj.mq2.co, `CO ppm concentration is ${obj.mq2.co.value}`);
+        
+        map_co = addValueToMap(map_co, obj.mq2.co.value);
+        map_timeCO = addValueToMap(map_timeCO, moment().format('LTS'));
 
         chartCO.update();
-
         isAtRisk = isAtRisk || aux;
     }
     if (pir_state) {
-        aux = DisplayPirMeasurement(obj.pir.movement);
-        if (map_pir.length >= max_sample) {
-            map_pir.shift();
-            map_timePir.shift();
-        }
-        map_pir.push(obj.pir.movement.value);
-        map_timePir.push(moment().format('LTS'));
-
+        aux = displayMeasurement('pir', obj.pir.movement, `Movement is ${obj.pir.movement.value}`);
+        
+        map_pir = addValueToMap(map_pir, obj.pir.movement.value);
+        map_timePir = addValueToMap(map_timePir, moment().format('LTS'));
+        
         chartPir.update();
-
         isAtRisk = isAtRisk || aux;
     }
 
