08cd4fcedf3607dc152ab1573f5cc8220543b3d0
Robert Murvai <robert.murvai@bullguard.com>
2023-09-11 23:05:26 +0300

    adapt GenerateMqtt_data.py

--- SerialReader/GenerateMqtt_data.py
+++ SerialReader/GenerateMqtt_data.py
@@ -6,9 +6,6 @@ import asyncio
 import paho.mqtt.client as mqtt
 from serial import Serial
 import random
-import requests
-
-ifttt_url = "https://maker.ifttt.com/trigger/{}/json/with/key/{}"
 
 class SensorReader:
     def __init__(self, port, baudrate, timeout, isTest):
@@ -40,6 +37,7 @@ class SensorReader:
             lpg = random.randint(0,1000)
             co = random.randint(0, 1000)
             smoke = random.randint(0, 1000)
+            pir = random.randint(0,1)
             # test = "{\\"dht22\\":{\\"temperature\\":{},\\"humidity\\":{}}},\\"mq2\\":{\\"lpg\\":{},\\"co\\":{},\\"smoke\\":{}}}".format(temp,humidity,lpg,co,smoke)
             test ={"dht22":
                    { "temperature":temp, 
@@ -50,6 +48,10 @@ class SensorReader:
                         "lpg":lpg,
                         "co":co,
                         "smoke":smoke
+                    },
+                    "pir": 
+                    { 
+                        "movement": pir
                     }
             }
 
@@ -73,30 +75,31 @@ class SensorReader:
 
 
 class WebPublisher:
-    def __init__(self, host, port, username, password, key):
+    def __init__(self, host, port, username, password):
         self.client = mqtt.Client()
         self.client.username_pw_set(username, password)
         self.client.connect(host, port)
-        self.key = key
 
-
-    def processData(self,data):
-        dict = {'dht22': 
-                      {'temperature': {'value': data["dht22"]["temperature"], 'risk': True if data["dht22"]["temperature"] < 8 or data["dht22"]["temperature"] > 35 else False}, 
-                       'humidity': {'value': data["dht22"]["humidity"], 'risk': True if data["dht22"]["humidity"] < 40 or data["dht22"]["humidity"] > 60 else False}
-                    }, 
-                    'mq2': {'lpg': {'value': data["mq2"]["lpg"], 'risk': False if data["mq2"]["lpg"] < 100 else True}, 
-                            'co': {'value': data["mq2"]["co"], 'risk': False if data["mq2"]["co"] < 100 else True},
-                            'smoke': {'value': data["mq2"]["smoke"], 'risk': False if data["mq2"]["smoke"] < 100 else True}
-                    }}
+    def processData(self, data):
+        dict = {'dht22':
+                {'temperature': {'value': data["dht22"]["temperature"],
+                                 'risk': True if data["dht22"]["temperature"] < 8 or data["dht22"][
+                    "temperature"] > 35 else False},
+                 'humidity': {'value': data["dht22"]["humidity"],
+                              'risk': True if data["dht22"]["humidity"] < 40 or data["dht22"][
+                     "humidity"] > 60 else False}
+                 },
+                'mq2': {'lpg': {'value': data["mq2"]["lpg"], 'risk': False if data["mq2"]["lpg"] < 100 else True},
+                        'co': {'value': data["mq2"]["co"], 'risk': False if data["mq2"]["co"] < 100 else True},
+                        'smoke': {'value': data["mq2"]["smoke"], 'risk': False if data["mq2"]["smoke"] < 100 else True}
+                        },
+                'pir': {'movement': {'value': data["pir"]["movement"], 'risk': data["pir"]["movement"]}}}
 
         return json.dumps(dict)
 
     async def publish(self, topic, data, retry_interval=5, max_retries=3):
         payload = json.dumps(data)
         processed_json = self.processData(data)
-        # logic to send to ifttt data
-        self.logicIfttt(processed_json)
 
         # logic to send data to mqtt
         for i in range(max_retries):
@@ -109,35 +112,6 @@ class WebPublisher:
         logging.error(
             "Max retries (%d) exceeded, giving up on publishing to %s", max_retries, topic)
         return False
-
-
-    def sendToIfttt(self, event_name, data):
-            return True
-            response = requests.post(ifttt_url.format(event_name, self.key), json=json.loads(data))
-            print(response.text)
-            return response.ok
-
-    def logicIfttt(self, data):
-        data = json.loads(data)
-        if data["dht22"]["temperature"]["risk"]:
-            self.sendToIfttt("temp_alert", json.dumps({'temperature': data["dht22"]["temperature"]["value"]}))
-        
-        if data["dht22"]["humidity"]["risk"]:
-            self.sendToIfttt("humidity_alert", json.dumps(
-                {'humidity': data["dht22"]["humidity"]["value"]}))
-
-        if data["mq2"]["lpg"]["risk"]:
-            self.sendToIfttt("lpg_alert", json.dumps(
-                {'lpg': data["mq2"]["lpg"]["value"]}))
-
-        if data["mq2"]["co"]["risk"]:
-            self.sendToIfttt("co_alert", json.dumps(
-                {'co': data["mq2"]["co"]["value"]}))
-
-        if data["mq2"]["smoke"]["risk"]:
-            self.sendToIfttt("smoke_alert", json.dumps(
-                {'smoke': data["mq2"]["smoke"]["value"]}))
-
 
 async def read_and_publish(sensor_reader, mqtt_publisher, topic):
     while True:
@@ -169,8 +143,7 @@ async def main():
         mqtt_publisher = WebPublisher(config["mqtt"]["host"],
                                        int(config["mqtt"]["port"]),
                                        config["mqtt"]["username"],
-                                       config["mqtt"]["password"],
-                                       config["ifttt"]["key"])
+                                       config["mqtt"]["password"])
 
         # Start read and publish tasks
         topic = config["mqtt"]["topic"]
